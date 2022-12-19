a8ee2f52e39db1013d13346a3b8d57ea3c5443cc
신석용 <syshin@cubox.ai>
2022-12-19 16:25:18 +0900

    디렉토리 변경

--- coco2yolo.py
+++ coco2yolo.py
@@ -5,17 +5,17 @@ import shutil
 
 import tqdm
 
-src_dir = os.path.join('data', 'IDcard_Detection')
-root = '../../data/IDCard_Detection'
-dst_dir = os.path.join(root, 'all')
+img_dir = os.path.join('data', 'idcard_trainset')
 annotation_path = os.path.join('data', 'IDcard_Detection.json')
+root = '../../data/idcard_detect'
+dst_img_dir = os.path.join(root, 'train')
 
 # Load coco format annotations
 with open(annotation_path, 'r', encoding='utf-8') as f:
     coco = json.load(f)
 
 # Check the number of images and number of annotations
-num_images = len(glob.glob(os.path.join(src_dir, '*.jpg')))
+num_images = len(glob.glob(os.path.join(img_dir, '*.jpg')))
 assert num_images == len(coco['images']) == len(coco['annotations'])
 print(f'전체 개수: {num_images}')
 
@@ -23,9 +23,9 @@ print(f'전체 개수: {num_images}')
 image_id_filename = {image['id']: image['file_name'] for image in coco['images']}
 
 # Copy yaml file
-os.makedirs(dst_dir, exist_ok=True)
 shutil.copy(os.path.join('yaml_files', 'idcard.yaml'), root)
 
+os.makedirs(dst_img_dir, exist_ok=True)
 for anno in tqdm.tqdm(coco['annotations'], 'coco2yolo'):
     # Check annotation
     assert len(anno['segmentation']) == 1, 'polygon이 1개가 아닙니다.'
@@ -43,10 +43,10 @@ for anno in tqdm.tqdm(coco['annotations'], 'coco2yolo'):
 
     # Copy image corresponding to annotation
     image_filename = image_id_filename[anno['image_id']]
-    shutil.copy(os.path.join(src_dir, image_filename),
-                os.path.join(dst_dir, image_filename))
+    shutil.copy(os.path.join(img_dir, image_filename),
+                os.path.join(dst_img_dir, image_filename))
 
     # Save yolo format annotation
-    anno_path = os.path.join(dst_dir, image_filename.replace('.jpg', '.txt'))
+    anno_path = os.path.join(dst_img_dir, image_filename.replace('.jpg', '.txt'))
     with open(anno_path, 'w', encoding='utf-8') as f:
         f.write(f'{category} {cx} {cy} {w} {h}')
